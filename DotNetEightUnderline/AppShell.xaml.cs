1313ff5b3e39ff0f4be7d7414d6aca100b408427
shanmugasundar-sf3429 <shanmugasundar.sekar@syncfusion.com>
2023-09-11 16:43:36 +0530

    Added Sample

---
+++ DotNetEightUnderline/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNetEightUnderline
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DotNetEightUnderline/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Maui.Controls.Shapes;
+
+namespace DotNetEightUnderline
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            AddPath();
+        }
+        void AddPath()
+        {
+            Line strikeOutLine = new Line();
+            strikeOutLine.BackgroundColor = Colors.Red;
+            strikeOutLine.StrokeThickness = 1;
+            MainLayout.SetLayoutBounds(strikeOutLine, new Rect(30, 30, 900, 5));
+            MainLayout.Add(strikeOutLine);
+        }
+    }
+
+}
